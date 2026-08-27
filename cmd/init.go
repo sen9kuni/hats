6186628d65199d02c0996e8e40075917dda9f896
prayogo santoso <prayogo.santoso681@gmail.com>
2026-08-27 23:57:21 +0700

    [feat]: command init and apply

--- cmd/init.go
+++ cmd/init.go
@@ -1,1 +1,25 @@
 package cmd
+
+import (
+	"fmt"
+
+	"github.com/sen9kuni/hats/internal/git"
+	"github.com/spf13/cobra"
+)
+
+var initCmd = &cobra.Command{
+	Use:   "init",
+	Short: "append out hook config to main git config",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		if err := git.EnsureHookExists("~/.config/hats/managed_includes.gitconfig"); err != nil {
+			return err
+		}
+
+		fmt.Println("sucess to append hook config")
+		return nil
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(initCmd)
+}
