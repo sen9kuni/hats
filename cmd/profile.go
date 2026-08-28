fb9c4445b42c2fa5b4f8246dfe469506fa366125
prayogo santoso <prayogo.santoso681@gmail.com>
2026-08-28 21:21:34 +0700

    [feat]: profile add

--- cmd/profile.go
+++ cmd/profile.go
@@ -1,1 +1,12 @@
 package cmd
+
+import "github.com/spf13/cobra"
+
+var profileCmd = &cobra.Command{
+	Use:   "profile",
+	Short: "Managing Git Profile",
+}
+
+func init() {
+	rootCmd.AddCommand(profileCmd)
+}
