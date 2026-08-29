49bff727ea356834d28256003da4c2c7d1d4843d
prayogo santoso <prayogo.santoso681@gmail.com>
2026-08-29 23:28:01 +0700

    [adjust]: change root cmd as import able for testing use

--- cmd/root.go
+++ cmd/root.go
@@ -7,14 +7,14 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var rootCmd = &cobra.Command{
+var RootCmd = &cobra.Command{
 	Use:               "Hats",
 	Short:             "A Git profile manager",
 	CompletionOptions: cobra.CompletionOptions{DisableDefaultCmd: true},
 }
 
 func Execute() {
-	if err := rootCmd.Execute(); err != nil {
+	if err := RootCmd.Execute(); err != nil {
 		os.Exit(1)
 	}
 }
