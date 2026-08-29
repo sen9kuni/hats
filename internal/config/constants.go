f31e7be606618abeaa81190a7953a93cffb7c458
prayogo santoso <prayogo.santoso681@gmail.com>
2026-08-29 23:28:47 +0700

    [feat]: global constant for fix issue typo

---
+++ internal/config/constants.go
@@ -0,0 +1,8 @@
+package config
+
+const (
+	IncludesFileName     = "managed_includes.gitconfig"
+	IncludesFileNameTemp = "managed_includes_tmp.gitconfig"
+	ProfileDirName       = "profile"
+	StateFileName        = "hats.toml"
+)
